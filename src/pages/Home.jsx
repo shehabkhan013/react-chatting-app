97c99273f5292ee388173472a7f3e28655c1fa67
A. M. Shehab Khan <shehabkhan013@gmail.com>
2024-08-28 13:01:17 +0600

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="w-full bg-[#FBFBFB]">
           <Userlist />
         </div>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-0 gap-x-0 lg:gap-x-5 xl:gap-x-10 md:pr-10">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-0 gap-x-3 lg:gap-x-5 xl:gap-x-10 md:pr-10">
           <div className="w-full h-full bg-white rounded-md">
             <FriendRequest />
           </div>
